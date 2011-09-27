af7ad1a58c7e0387fbda955683a0985aef79f9f8
jordandmc <jordandmc@gmail.com>
2011-09-27 01:56:18 -0500

    Refactored EListener to ModListener

--- LevelBuff/src/LevelBuff.java
+++ LevelBuff/src/LevelBuff.java
@@ -1,7 +1,10 @@
+import org.bukkit.event.Event;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class LevelBuff extends JavaPlugin{
-
+	private final MobListener playerListener = new MobListener(this);
+	
 	@Override
 	public void onDisable() {
 		// TODO Auto-generated method stub
@@ -10,7 +13,8 @@ public class LevelBuff extends JavaPlugin{
 
 	@Override
 	public void onEnable() {
-		// TODO Auto-generated method stub
+		PluginManager pm = getServer().getPluginManager();
+		pm.registerEvent(Event.Type.PLAYER_PICKUP_ITEM, playerListener, Event.Priority.Normal, this);
 		
 	}
 }
---
+++ LevelBuff/src/MobListener.java
@@ -0,0 +1,20 @@
+import net.minecraft.server.EntityExperienceOrb;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.entity.EntityListener;
+
+public class MobListener extends EntityListener{
+	private LevelBuff plugin;
+	
+	public MobListener(LevelBuff instance){
+		plugin = instance;
+		
+		
+	}
+	
+	@Override
+	public void onEntityDeath(EntityDeathEvent event){
+		if(event.getEntity() instanceof EntityExperienceOrb){
+			
+		}
+	}
+}
