b8ed04a1587ede451403eb286fe218132c4a2ed4
jordandmc <jordandmc@gmail.com>
2011-09-26 20:48:10 -0500

    extend JavaPlugin

--- LevelBuff/src/LevelBuff.java
+++ LevelBuff/src/LevelBuff.java
@@ -1,4 +1,16 @@
+import org.bukkit.plugin.java.JavaPlugin;
 
-public class LevelBuff {
+public class LevelBuff extends JavaPlugin{
 
+	@Override
+	public void onDisable() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onEnable() {
+		// TODO Auto-generated method stub
+		
+	}
 }
