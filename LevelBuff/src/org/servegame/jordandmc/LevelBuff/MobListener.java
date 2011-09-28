0c9e84d66f51f53bd7ed28616760e492912bf475
chrisjamieson <chrisjamieson@192.168.0.100>
2011-09-27 19:30:45 -0700

    Renamed to MobListener

--- LevelBuff/src/org/servegame/jordandmc/LevelBuff/MobListener.java
+++ LevelBuff/src/org/servegame/jordandmc/LevelBuff/MobListener.java
@@ -4,10 +4,10 @@ import net.minecraft.server.EntityExperienceOrb;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.EntityListener;
 
-public class ModListener extends EntityListener{
+public class MobListener extends EntityListener{
 	private LevelBuff plugin;
 	
-	public ModListener(LevelBuff instance){
+	public MobListener(LevelBuff instance){
 		plugin = instance;
 		
 		
